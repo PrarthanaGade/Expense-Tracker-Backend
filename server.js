acbdcf5a47f6f46cc11c09f3832df9725dd9e5c6
GeniusPrarthana <Prarthanagade25@gmail.com>
2025-06-12 22:19:40 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
